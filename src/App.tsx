a71da921d4ea243fc95c4f0584fb0676998ed80f
rhymes17 <devrahul1709@gmail.com>
2025-01-21 14:33:34 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 className="text-3xl text-red-500">Hello world</h1>
+      <h1 className="text-3xl text-red-500">Hello </h1>
     </div>
   );
 }
