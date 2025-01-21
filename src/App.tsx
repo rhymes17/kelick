dbcc508c50086338cb1cb2019838510da03fb794
rhymes17 <devrahul1709@gmail.com>
2025-01-21 17:39:02 +0530

    poc: file upload functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import FileUpload from './components/FileUpload';
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl text-red-500">Hello world</h1>
+      <FileUpload />
     </div>
   );
 }
